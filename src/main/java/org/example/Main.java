0362a5cd0f6919b4bd20de1f07ff3288307632c0
Jeffery Tse <kytseag@ust.hk>
2024-07-17 11:59:25 +0800

    feat: add GET method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,36 @@
 package org.example;
 
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        URL url = new URL("https://cat-fact.herokuapp.com/facts/");
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+//        con.setRequestProperty("Content-Type", "application/json");
+
+//        Map<String, String> parameters = new HashMap<>();
+//        parameters.put("foo1", "bar1");
+//        parameters.put("foo2", "bar2");
+
+        int responseCode = con.getResponseCode();
+        System.out.println("response code: " + responseCode);
+
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+            System.out.println(response);
+        } else {
+            System.out.println("GET request failed");
+        }
+
+        con.disconnect();
     }
 }
